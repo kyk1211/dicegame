0702ade7d663470f6742d3f4c1aff9bc3e40015b
곽윤구 <advsdvs@gmail.com>
2021-08-12 21:44:59 +0900

    props.children

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import Button from './Button';
 import Dice from './Dice';
+ 
 
 function App() {
   return (
     <div>
+      <div>
+        <Button>throw</Button>
+        <Button>go first</Button>
+      </div>
       <Dice color='red' num={2} />
     </div>
   );
