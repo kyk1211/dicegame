fca15454fe68bd724e5753d70c237ab98e031e53
곽윤구 <advsdvs@gmail.com>
2021-08-14 15:34:44 +0900

    state(2)

--- src/App.js
+++ src/App.js
@@ -8,14 +8,20 @@ function random(n) {
 
 function App() {
   const [num, setNum] = useState(1);
+  const [sum, setSum] = useState(0);
+  const [gameHistory, setGameHistory] = useState([]);
 
   const handleRollClick = () => {
     const nextNum = random(6);
     setNum(nextNum);
+    setSum(sum + nextNum);
+    setGameHistory([...gameHistory,nextNum]);
   };
 
   const handleClearClick = () => {
     setNum(1);
+    setSum(0);
+    setGameHistory([]);
   }
 
   return (
@@ -24,7 +30,14 @@ function App() {
         <Button onClick={handleRollClick}>Throw</Button>
         <Button onClick={handleClearClick}>Go first</Button>
       </div>
-      <Dice color='red' num={num} />
+      <div>
+        <h2>me</h2>
+        <Dice color='blue' num={num} />
+        <h2>score</h2>
+        <p>{sum}</p>
+        <h2>Record</h2>
+        <p>{gameHistory.join(', ')}</p>
+      </div>
     </div>
   );
 }
